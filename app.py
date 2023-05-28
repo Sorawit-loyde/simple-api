d9bf53f5b355022885fa9210989f1f3e05d46999
Rathachai CHAWUTHAI <rathachai.c@gmail.com>
2023-05-28 11:49:54 +0700

    Create app.py

---
+++ app.py
@@ -0,0 +1,15 @@
+
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/')
+def inex():
+    return "Index!"
+
+@app.route('/hello/<name>', methods=['GET'])
+def hello(name):
+    return "Hello, " + name
+
+if __name__ == '__main__':
+    app.run()
