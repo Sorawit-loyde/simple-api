cce0947070b4c714180e55c735ac765fc4488269
Rathachai CHAWUTHAI <rathachai.c@gmail.com>
2023-06-26 09:58:07 +0700

    validate input and return an error message

--- app/app.py
+++ app/app.py
@@ -14,14 +14,19 @@ def hello(name):
 
 @app.route('/calculate/<num1>/<num2>', methods=['GET'])
 def calculate(num1, num2):
-    num1 = eval(num1)
-    num2 = eval(num2)
-    results = {
-            'plus' : num1 + num2,
-            'minus' : num1 - num2,
-            'multiply': num1 * num2,
-            'divide' : num1/num2
-        }
+    try:
+        num1 = eval(num1)
+        num2 = int(num2)
+
+        results = {
+                'plus' : num1 + num2,
+                'minus' : num1 - num2,
+                'multiply': num1 * num2,
+                'divide' : num1/num2
+            }
+    except:
+        results = { 'error_msg' : 'inputs must be numbers' }
+
     return jsonify(results)
 
 
