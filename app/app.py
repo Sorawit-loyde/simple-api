ce4f8f4f10d1ba3d13845d899e304c2bd09aaa5b
Rathachai CHAWUTHAI <rathachai.c@gmail.com>
2023-06-26 09:50:03 +0700

    add a calculate function

--- app/app.py
+++ app/app.py
@@ -1,5 +1,5 @@
 
-from flask import Flask
+from flask import Flask, jsonify
 
 app = Flask(__name__)
 
@@ -10,6 +10,20 @@ def index():
 @app.route('/hello/<name>', methods=['GET'])
 def hello(name):
     return "Hello, " + str(name)
+
+
+@app.route('/calculate/<num1>/<num2>', methods=['GET'])
+def calculate(num1, num2):
+    num1 = eval(num1)
+    num2 = eval(num2)
+    results = {
+            'plus' : num1 + num2,
+            'minus' : num1 - num2,
+            'multiply': num1 * num2,
+            'divide' : num1/num2
+        }
+    return jsonify(results)
+
 
 if __name__ == '__main__':
     app.run()
